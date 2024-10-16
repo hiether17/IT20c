ebcb1a9921b1ec73bd139dd5ce1a8533bf2b1080
hiether17 <lhietherfhiona@gmail.com>
2024-10-16 22:32:14 +0800

    Added class and function to Costumer.java

--- Queue/Costumer.java
+++ Queue/Costumer.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Queue;
 
 /**
@@ -10,5 +6,15 @@ package Queue;
  * @author HP
  */
 public class Costumer {
-    
+
+    private String name;
+
+    public Costumer(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
+
