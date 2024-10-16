15ca5478f27987dbbe53860b0f408b28703b1652
hiether17 <lhietherfhiona@gmail.com>
2024-10-16 10:48:12 +0800

    Added some changes on Display Via for loop package

--- Array/DisplayViaForLoop.java
+++ Array/DisplayViaForLoop.java
@@ -1,14 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Array;
 
-/**
- *
- * @author HP
- */
 public class DisplayViaForLoop {
     
+    public static void main(String[] args){
+       
+        //declaring variable to array
+        String[] SeventeenHitSongs = {"Clap", "Maestro", "Very Nice", "Super", "LALALI", "HOT"};
+        
+        //procedure
+        
+        for (int i = 0; i < SeventeenHitSongs.length; i++){
+            System.out.println(SeventeenHitSongs[i]);
+        }
+    }
+    
 }
+
+//output
+/*
+Clap
+Maestro
+Very Nice
+Super
+LALALI
+HOT
+*/
+
