94468d47623e0fcd7bbef7347c3bf6b86a31fda0
hiether17 <lhietherfhiona@gmail.com>
2024-10-16 10:52:05 +0800

    Added inputs for the array

---
+++ Array/Multidimentional_Array.java
@@ -0,0 +1,14 @@
+
+package Array;
+
+public class Multidimentional_Array {
+    public static void main(String[] args) {
+        //inputs for the array
+        String [][] arrays = {
+            {".", "?", "<", ">", "/"},
+            {"/", "0", ",", "<", ">"},
+            {".", "-", "0", "/", "?"},
+            {"<", ",", "?", "-", ">"} 
+        };
+    }
+}
