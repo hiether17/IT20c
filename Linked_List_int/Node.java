5c0ff1d734c1f32ae2c5a0d2c4221c3718e4df03
hiether17 <lhietherfhiona@gmail.com>
2024-10-16 12:05:07 +0800

    Adding method for adding item,delete,move/swap item

---
+++ Linked_List_int/Node.java
@@ -0,0 +1,17 @@
+
+package Linked_List_int;
+
+/**
+ *
+ * @author HP
+ */
+public class Node {
+      	int data;  
+    Node next;  
+
+    // Constructor
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
