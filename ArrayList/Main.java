9a76d26092b80b9278540d86a4cc643e864f934c
hiether17 <lhietherfhiona@gmail.com>
2024-10-16 11:16:10 +0800

    Display the updated item

--- ArrayList/Main.java
+++ ArrayList/Main.java
@@ -6,7 +6,7 @@ public class Main {
     
     public static void main(String[] args){
         
-        Scanner sc = new Scanner(System.in);
+   Scanner sc = new Scanner(System.in);
         //Creating object of the class to access all the method on the ItemManager
         ItemManager list = new ItemManager();
         
@@ -16,7 +16,14 @@ public class Main {
         //Display the list
         list.displayItems();
         
-         //To read and store the input in the variable named index
+        //To read and store the input in the variable named item
+        System.out.println("Enter an item you want to add: ");
+        item = sc.nextLine();
+        //Displays the new Item
+        list.addName(item);
+        System.out.println(" ");
+        
+        //To read and store the input in the variable named index
         System.out.println("Enter an index number that you want to change: ");
         index = sc.nextInt();
         //Calling so that it reads the input of the next (String) line
@@ -24,7 +31,18 @@ public class Main {
         //To read and store the input in the variable named newItem
         System.out.println("Enter a new name that you want to change on that index number: ");
         newItem = sc.nextLine();
+        //Displays the updated item
+        list.updateItem(index, newItem);
+        list.displayItems();
+        System.out.println(" ");
+        
+        //To read and store the input in the variable named findItem
+        System.out.println("Enter the name that you want to find: ");
+        finditem = sc.nextLine();
+        //Display the item you want to find
+        list.findItem(finditem);
+        list.displayItems();
+        System.out.println(" ");
         
     }
-} 
-
+}
