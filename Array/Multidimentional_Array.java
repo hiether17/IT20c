62cdd9a17ce2ddaabe2bfe1c36e3e98200ecefc4
hiether17 <lhietherfhiona@gmail.com>
2024-10-16 10:53:47 +0800

    Added a nested for loop to make the array multidimentional

--- Array/Multidimentional_Array.java
+++ Array/Multidimentional_Array.java
@@ -10,5 +10,13 @@ public class Multidimentional_Array {
             {".", "-", "0", "/", "?"},
             {"<", ",", "?", "-", ">"} 
         };
+        // a nested for loop to make the array multidimensional 
+        for (int i=0; i < arrays.length; i++){
+            for (int j=0; j < arrays[i].length; j++){
+                System.out.println(arrays[i][j]);
+            }
+            System.out.println(" ");
+        }
+
     }
 }
